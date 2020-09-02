243b867a7f5a366928fcb08666da575d6718ddc4
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-02 13:02:05 -0500

    saving

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ In this project we will learn how to create a very simple Web API using `Node.js
 - [ ] type `npm i` to download dependencies.
 
 Please follow along as the instructor creates and adds `middleware` to the API step by step.
+
+Types of middleware
+
+built in - no need to install
+third party - npm install
+custom - we build
+
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express'); // importing a CommonJS module
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
+const helmet = require("helmet");
+
 const server = express();
 
-server.use(express.json());
+//global middleware
+server.use(express.json());//built in middlware
+server.use(helmet());// third party security middleware
+server.use(logger())//custom middleware
 
-server.use('/api/hubs', hubsRouter);
+server.use('/api/hubs', gate('mellon'), hubsRouter);
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
@@ -16,5 +21,29 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+server.get('/toss', flip, (req, res) => {
+res.status(200).json({ toss: req.coin ? 'Heads' : 'Tails'})
+})
+
+function logger() {
+  return function(req, res, next) {
+    console.log(`a ${req.method} request was made to ${req.url}`);
+    next()
+}
+}
+
+function flip(req, res, next) {
+const flip = Math.round(Math.random());
+flip === 1 ? req.coin = true : req.coin = false;
+next()
+}
+
+//gate middleware will check passwords to make sure they match, headers and provided password
+function gate(password) {
+  return function (req, res, next) {
+    req.headers.password === password ? next() : res.status(401).json({ error: 'incorrect password' })
+  }
+}
 
 module.exports = server;
